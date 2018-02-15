1321a80f0f1489e218d8230cbee48f88036400a9
arashikodoku <filip.krol32@gmail.com>
2018-02-15 16:41:10 +0100

    moje zmiany

---
+++ app/src/main/java/com/example/administrator/kasprzakmemes/EkranStartowy.java
@@ -0,0 +1,13 @@
+package com.example.administrator.kasprzakmemes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EkranStartowy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekran_startowy);
+    }
+}
